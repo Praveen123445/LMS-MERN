3f91bf1bbd0c1d4d218c3ce1d065c5d1948380c2
Praveen Verma <Pravinvarma324@gmail.com>
2025-01-21 09:38:21 +0530

    adding dist

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL : 'http://localhost:5000',
-});
+    baseURL : import.meta.env.VITE_SERVER_URL,
+})
 
 axiosInstance.interceptors.request.use(config=> {
     const accessToken = JSON.parse(sessionStorage.getItem('accessToken')) || "";
